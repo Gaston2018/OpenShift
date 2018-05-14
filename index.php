05de44801894d37eabdd465db629753daef98e2b
Gaston2018 <38983253+Gaston2018@users.noreply.github.com>
2018-05-14 11:09:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Prueba</title>
+	  <style type="text/css">
+  body {
+	color: purple;
+    background-color: lightblue }
+  </style>
+
+    <link rel="stylesheet" href="./css/jquery.flipster.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
+
+</head>
+<body>
+
+<div class="flipster">
+    <ul>
+    </ul>
+</div>
+
+<p class="stats"></p>
+
+
+<script>window.$ = window.jQuery = require('./js/jquery.min.js');</script>
+<script src="./js/jquery.flipster.min.js"></script>
+<script src="./js/script.js"></script>
+
+    <ul>
+	
+	  </ul>
+</body>
+</html>
