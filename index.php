5a5d2011b922c8ed81a9db72c1474a4c14622033
Gaston2018 <38983253+Gaston2018@users.noreply.github.com>
2018-05-15 14:24:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
+<?php
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+echo "Hola Mundo";
 
-    <title>Backup de Aplicaciones</title>
-	  <style type="text/css">
-  body {
-	color: purple;
-    background-color: lightblue }
-  </style>
-
-    <link rel="stylesheet" href="./css/jquery.flipster.min.css">
-    <link rel="stylesheet" href="./css/styles.css">
-
-</head>
-<body>
-
-<div class="flipster">
-    <ul>
-    </ul>
-</div>
-
-<p class="stats"></p>
-
-
-<script>window.$ = window.jQuery = require('./js/jquery.min.js');</script>
-<script src="./js/jquery.flipster.min.js"></script>
-<script src="./js/script.js"></script>
-
-    <ul>
-	<input type="submit" value="GCPAAP234" onclick = "location='Apps GCPAAP234.html'"/>
-	<input type="submit" value="GCPAAP235" onclick = "location='Apps GCPAAP235.html'"/>
-    </ul>
-</body>
-</html>
+?>
